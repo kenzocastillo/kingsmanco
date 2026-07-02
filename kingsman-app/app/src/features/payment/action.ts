7b30899e8d701af1f4db3a56e23607f726b50c46
kvcastillo <kenzovcastillo@gmail.com>
2026-07-03 07:39:00 +0800

    fix:error in checkout

--- kingsman-app/app/src/features/payment/action.ts
+++ kingsman-app/app/src/features/payment/action.ts
@@ -1,28 +1,38 @@
 "use server";
-import { Product } from "@/app/types/types";
-import Stripe from "stripe";
 
+import Stripe from "stripe";
 import { createOrder } from "../orders/actions";
+import { Product } from "@/app/types/types";
 
 export async function handleCheckout(cartItems: Product[]) {
-  const order = await createOrder();
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+  try {
+    const order = await createOrder();
 
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    line_items: cartItems.map((item) => ({
-      price_data: {
-        currency: "php",
-        product_data: { name: item.name },
-        unit_amount: item.price * 100,
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: cartItems.map((item) => ({
+        price_data: {
+          currency: "php",
+          product_data: {
+            name: item.name,
+          },
+          unit_amount: item.price * 100,
+        },
+        quantity: item.quantity,
+      })),
+      mode: "payment",
+      success_url: `${process.env.NEXT_PUBLIC_URL}/orders/${order.id}?success=true`,
+      cancel_url: `${process.env.NEXT_PUBLIC_URL}/cart`,
+      metadata: {
+        orderId: order.id,
       },
-      quantity: item.quantity,
-    })),
-    mode: "payment",
-    success_url: `${process.env.NEXT_PUBLIC_URL}/orders/${order.id}?success=true`,
-    cancel_url: `${process.env.NEXT_PUBLIC_URL}/cart`,
-    metadata: { orderId: order.id },
-  });
+    });
 
-  return session.url;
+    return session.url;
+  } catch (err) {
+    console.error("HANDLE CHECKOUT ERROR:", err);
+    throw err;
+  }
 }
